834437d27030d735ded0a4ab7f4f3f8e4f2bd91f
cesar mota <cesarmota@ymail.com>
2019-02-19 13:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,20 +14,7 @@ var line=katzDeliLine.shift();
   
     }
  
-<<<<<<< HEAD
-var currentLine = function (katzDeliLine)
-{
-  if (katzDeliLine.length===0)
-  {  return "The line is currently empty."}
-  
-  var nameAndNumber=[];
-    for ( var i=0; i<katzDeliLine.length;i++)
-  { nameAndNumber.push(i+1 +  ". " + katzDeliLine[i])
-    
-  }
-   return "The line is currently: " +nameAndNumber.join(", ");
-}
-=======
+
 var currentLine = function (katzDeliLine) {
   if (katzDeliLine.length===0) {
   return "The line is currently empty.";
