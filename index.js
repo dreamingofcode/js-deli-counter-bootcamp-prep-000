4a09370b8a06104e9f62ed1684a188c66bb61c15
cesar mota <cesarmota@ymail.com>
2019-02-19 13:57:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var line=katzDeliLine.shift();
   
     }
  
+<<<<<<< HEAD
 var currentLine = function (katzDeliLine)
 {
   if (katzDeliLine.length===0)
@@ -26,3 +27,15 @@ var currentLine = function (katzDeliLine)
   }
    return "The line is currently: " +nameAndNumber.join(", ");
 }
+=======
+var currentLine = function (katzDeliLine) {
+  if (katzDeliLine.length===0) {
+  return "The line is currently empty.";
+}
+   var customerAndNumber = [];
+   for (var i=0; i<katzDeliLine.length; i++) {
+   customerAndNumber.push(i+1 + ". " + katzDeliLine[i])
+}
+  return "The line is currently: " + customerAndNumber.join(", ");
+}
+>>>>>>> 62b6658e581de4b8fee9756c119ac30293c9bc6c
